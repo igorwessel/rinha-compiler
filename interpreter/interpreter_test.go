857f981953d543051775ef3f822bd655c18bc638
Igor Wessel <igor_wessel@hotmail.com>
2023-09-09 06:14:24 -0300

    testing...

--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -6,4 +6,12 @@ import (
 
 func TestBasicPlus(t *testing.T) {
 
+	text := "1+1"
+  interpreter := Interpreter{Tokenizer{text, 0, nil}, &Token{}}
+	result := interpreter.Expr()
+
+	if result != 2 {
+		t.Fatalf(`should be 2 in basic sum expression like 1+1, we receive %q`, result)
+	}
 }
+

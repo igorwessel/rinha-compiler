cae9baea7f9129b2332132d14547579c1d6c42d3
Igor Wessel <igor_wessel@hotmail.com>
2023-09-11 18:46:06 -0300

    feat: tests for Number/Bool/String

--- interpreter.test.ts
+++ interpreter.test.ts
@@ -1,19 +1,47 @@
-import { it, expect, spyOn, describe } from "bun:test";
+import { it, expect, spyOn, describe, afterEach } from "bun:test";
 
-import {File, evaluate} from './interpreter'
+import { File, evaluate } from './interpreter'
 import { readFileSync } from "fs";
 
 const getAST = (file: string) => {
-    const json = readFileSync(`./examples/${file}.json`, { encoding: 'utf-8'})
+    const json = readFileSync(`./examples/${file}.json`, { encoding: 'utf-8' })
     const ast: File = JSON.parse(json)
 
     return ast
 }
 
+const spy = spyOn(console, "log")
+
+afterEach(() => {
+    spy.mockClear()
+})
+
 describe("Print", () => {
+
+    it("String", () => {
+        const ast = getAST("string")
+
+        evaluate(ast.expression)
+        expect(spy).toHaveBeenCalled()
+        //@ts-ignore
+        expect(spy.mock.calls[0][0]).toEqual("rinha")
+    })
+
+    it("Bool", () => {
+        const ast = getAST('bool')
+
+        evaluate(ast.expression)
+
+        expect(spy).toHaveBeenCalled()
+        //@ts-ignore
+        expect(spy.mock.calls[0][0]).toEqual(true)
+    })
+
+    it.todo("Tuple")
+    it.todo("Closure")
+
     it("Number", () => {
-        const spy = spyOn(console, "log")
-        const ast = getAST('print')
+        const ast = getAST('number')
 
         evaluate(ast.expression)
 
